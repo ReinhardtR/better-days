d6e6d9282b31ed39f4afe48d778c4514ae6efbf3
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-02-15 07:45:36 +0100

    add simple footer placeholder

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,9 @@
 export function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="mt-16 flex items-center justify-center border-t border-t-slate-200 p-8 dark:border-t-slate-700">
+      <p className="text-xl font-medium text-slate-500 dark:text-slate-400">
+        Better Days
+      </p>
+    </footer>
+  );
 }
